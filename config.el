2c9029b6f36f7828cb064a1906b1ce100b054e66
mlmulv <bpoykin@gmail.com>
2024-02-27 23:46:00 -0500

    Added neotree and changed to org-bullets

--- config.el
+++ config.el
@@ -7,6 +7,14 @@
  (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
  (load-theme 'dracula t)
 
+(add-to-list 'load-path "~/.emacs.d/neotree")
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+(add-to-list 'load-path "~/.emacs.d/org-bullets")
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
 (after! doom-themes
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t))
